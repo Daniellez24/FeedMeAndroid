669c92fabc77262f46278f013e09931e1f372332
Tamir Abutbul <1tamir198@gmail.com>
2023-01-09 18:47:39 +0200

    Add blank ui for fact fragments

--- app/src/main/java/com/example/feedme/FeedFragment.java
+++ app/src/main/java/com/example/feedme/FeedFragment.java
@@ -12,6 +12,7 @@ public class FeedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_feed, container, false);
     }
 
