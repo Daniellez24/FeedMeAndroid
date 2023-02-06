f252ab6e2589ad6f79af52af27faef2c7217269c
Tamir <1tamir198@gmail.com>
2023-02-06 13:36:55 +0200

    Adding app context

--- app/src/main/java/com/example/feedme/models/Model.java
+++ app/src/main/java/com/example/feedme/models/Model.java
@@ -1,21 +1,22 @@
 package com.example.feedme.models;
 
 import android.graphics.Bitmap;
+import android.util.Log;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
-
-import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Model {
     private static final Model _instance = new Model();
-
+    private Executor executor = Executors.newSingleThreadExecutor();
     private FirebaseModel firebaseModel = new FirebaseModel();
+    AppLocalDbRepository localDb = AppLocalDb.getAppDb();
 
     public static Model instance(){
         return _instance;
@@ -80,19 +81,18 @@ public class Model {
         return firebaseModel.getCurrentUserId();
     }
 
-
     public void uploadImage(String name, Bitmap bitmap, Listener<String> listener){
         firebaseModel.uploadImage(name, bitmap, listener);
     }
 
 
-    private LiveData<List<Recipe>> recipeList =  new MutableLiveData<>();
+    private LiveData<List<Recipe>> recipeList = null;
 
     public LiveData<List<Recipe>> getFeedItems(){
-        //TODO pull this out of local db if recipeList is null and not
-        // initialize it to MutableLiveData
+
+        recipeList = localDb.recipeDao().getAll();
+
         if (recipeList == null) {
-            recipeList = new MutableLiveData<>();
             refreshRecipes();
         }
         return recipeList;
@@ -103,6 +103,13 @@ public class Model {
         firebaseModel.getFeedItems(new Listener() {
             @Override
             public void onComplete(Object data) {
+                Log.d("danilelee", data + "");
+                executor.execute(() -> {
+//            localDb.recipeDao().insertAll(new Recipe("gA2lpMhGJTf8EMEFfaO5zxjwEZdK2","https://firebasestorage.googleapis.com/v0/b/feedme-android.appspot.com/o/images%2FSat%20Jan%2021%2015%3A21%3A47%20GMT%2B02%3A00%202023.jpg?alt=media&token=3761124f-e31b-4800-a481-fb48ac74c529","make pasta","sdasdas"));
+//            localDb.recipeDao().insertAll(new Recipe("gA2lpMhGJTf8EMEFfaO5zxjwEZsdK2","https://firebasestorage.googleapis.com/v0/b/feedme-android.appspot.com/o/images%2FSat%20Jan%2021%2015%3A21%3A47%20GMT%2B02%3A00%202023.jpg?alt=media&token=3761124f-e31b-4800-a481-fb48ac74c529","make pasta","sdasdas"));
+//            localDb.recipeDao().insertAll(new Recipe("gA2lpMhGJTf8EMEFfaO5zxjwEZsK2","https://firebasestorage.googleapis.com/v0/b/feedme-android.appspot.com/o/images%2FSat%20Jan%2021%2015%3A21%3A47%20GMT%2B02%3A00%202023.jpg?alt=media&token=3761124f-e31b-4800-a481-fb48ac74c529","make pasta","sdasdas"));
+                });
+
             }
         });
     }
